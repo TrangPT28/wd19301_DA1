e2e2bcefee03a0676663940759b394e5f3b01846
trangpt <trangptph53066@gmail.com>
2024-11-08 08:09:25 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// HÀM
+// Arrow function (hàm mũi tên)
+// function tinh tong(a,b{
+//     return a+b;
+// })
+
+// const tinhtong 
+// const tenham = a=>console.log(a);
+// const tenham = ()=>console.log(`jab`);
+
+// Cú pháp IIFE
+// ((a,b)=>console.log(a+b))(5,6);
+
+
+// Tham số mặc định
+// const tinhtong=(a,b=0)=>{
+//     return a+b;
+// }
+// console.log(tinhtong(6)); // 6
+
+// Hàm callback => Hàm đóng vai trò là 1 tham số của hàm
+// const tinhtong = (a,b)=>a+b;
+// const display (a,b, callbackFn) => {
+//     console.log (`Tong ${a}+${b} = ${callbackFn(a,b)}`);
+// }
+// display (6,5, tinhtong)
+
